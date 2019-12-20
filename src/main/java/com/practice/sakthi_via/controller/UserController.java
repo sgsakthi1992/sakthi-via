bd4aeeffc64ab2df5765cdc83a83f7d50a7bf018
sgsakthi1992 <sgsakthi1992@gmail.com>
2019-12-20 16:52:41 +0100

    added code for Post

--- src/main/java/com/practice/sakthi_via/controller/UserController.java
+++ src/main/java/com/practice/sakthi_via/controller/UserController.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.practice.sakthi_via.model.Users;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/users")
@@ -21,17 +23,13 @@ public class UserController {
 
     @RequestMapping(value = "/getUsers", method = RequestMethod.GET)
     public ResponseEntity<List> getUsers() {
-        Users user = new Users();
-        user.setName("Gokula Sakthi");
-        user.setUsername("Sakthi");
-        user.setEmail("sgsakthi1992@gmail.com");
-        userRepository.save(user);
         List<Users> usersList = userRepository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(usersList);
     }
 
     @RequestMapping(value = "/createUser", method=RequestMethod.POST)
-    public void createUser(){
-
+    public Optional<Users> createUser(@RequestBody Users user){
+        userRepository.save(user);
+        return userRepository.findById(user.getId());
     }
 }
