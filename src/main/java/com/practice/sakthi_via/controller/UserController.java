48c18948a68b2150cd99ef39c985078c1461ced0
malli <mallikarjun.bandi@gmail.com>
2019-12-25 16:42:05 +0100

    update user

--- src/main/java/com/practice/sakthi_via/controller/UserController.java
+++ src/main/java/com/practice/sakthi_via/controller/UserController.java
@@ -3,17 +3,18 @@ package com.practice.sakthi_via.controller;
 import com.practice.sakthi_via.exception.ResourceNotFoundException;
 import com.practice.sakthi_via.model.Users;
 import com.practice.sakthi_via.repository.UserRepository;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.contains;
@@ -25,42 +26,42 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping(value = "/getUsers", method = RequestMethod.GET)
+    @GetMapping("/getUsers")
     public ResponseEntity<List> getUsers() {
         List<Users> usersList = userRepository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(usersList);
     }
 
-    @RequestMapping(value = "/createUser", method = RequestMethod.POST)
+    @PostMapping("/createUser")
     public ResponseEntity<Users> createUser(@RequestBody Users user) {
         userRepository.save(user);
         Users newUser = userRepository.findById(user.getId()).get();
         return ResponseEntity.status(HttpStatus.OK).body(newUser);
     }
 
-    @RequestMapping(value = "/getUserById/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/getUserById/{id}")
     public ResponseEntity<Users> getUserById(@PathVariable(value = "id") Integer id) throws ResourceNotFoundException {
         Users user = userRepository.findById(id).orElseThrow(
-                () -> new ResourceNotFoundException("Employee ID " + id + " not found"));
+                () -> new ResourceNotFoundException("User ID " + id + " not found"));
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
-    @RequestMapping(value = "/deleteUserById/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping("/deleteUserById/{id}")
     public ResponseEntity<String> deleteById(@PathVariable(value = "id") Integer id) throws ResourceNotFoundException {
         Users user = userRepository.findById(id).orElseThrow(
-                () -> new ResourceNotFoundException("Employee ID " + id + " not found"));
+                () -> new ResourceNotFoundException("User ID " + id + " not found"));
         userRepository.deleteById(id);
         return ResponseEntity.status(HttpStatus.OK).body("Success");
     }
 
-    @RequestMapping(value = "/getUserByEmail/{email}", method = RequestMethod.GET)
+    @GetMapping("/getUserByEmail/{email}")
     public ResponseEntity<List> getUserById(@PathVariable(value = "email") String email) throws ResourceNotFoundException {
         List<Users> usersList = userRepository.findByEmail(email).orElseThrow(
                 () -> new ResourceNotFoundException("Email " + email + " not found"));
         return ResponseEntity.status(HttpStatus.OK).body(usersList);
     }
 
-    @RequestMapping(value = "/getUserByUsernameOrEmail", method = RequestMethod.GET)
+    @GetMapping("/getUserByUsernameOrEmail")
     public ResponseEntity<List> getUserByUsernameOrEmail(Users user) {
         ExampleMatcher exampleMatcher = ExampleMatcher.matchingAny()
                 .withMatcher("email", contains().ignoreCase())
@@ -69,6 +70,17 @@ public class UserController {
 
         List<Users> usersList = userRepository.findAll(example);
         return ResponseEntity.status(HttpStatus.OK).body(usersList);
+    }
+
+    @ApiOperation(value = "Update an employee")
+    @PutMapping("/updateUser/{id}")
+    public ResponseEntity<Users> updateUser(
+            @ApiParam(value = "User Id to update user object", required = true) @PathVariable(value = "id") Integer id,
+            @ApiParam(value = "Update User object", required = true) @Valid @RequestBody Users userDetails) throws ResourceNotFoundException {
+        Users user = userRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("User ID " + id + " not found"));
+        Users updatedUser = userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedUser);
     }
 
 }
