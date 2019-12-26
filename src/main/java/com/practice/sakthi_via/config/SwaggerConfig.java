9f277a4427ae223ad57b18242b80019dac566322
malli <mallikarjun.bandi@gmail.com>
2019-12-26 14:05:46 +0100

    refactoring

--- src/main/java/com/practice/sakthi_via/config/SwaggerConfig.java
+++ src/main/java/com/practice/sakthi_via/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select().apis(RequestHandlerSelectors.basePackage("com.practice.sakthi_via"))
-                .paths(PathSelectors.regex("/employee.*"))
+                .paths(PathSelectors.regex("/api/v1.*"))
                 .build();
     }
 }
