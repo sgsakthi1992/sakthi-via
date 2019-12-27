315160a3a266c37332c84fab74913a675c83d383
malli <mallikarjun.bandi@gmail.com>
2019-12-27 20:57:56 +0100

    exception handler

--- src/main/java/com/practice/sakthi_via/exception/GlobalExceptionHandler.java
+++ src/main/java/com/practice/sakthi_via/exception/GlobalExceptionHandler.java
@@ -4,11 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
     public final ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException e){
