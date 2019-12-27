cef771ec3cd02acfe80f6083792aff7a89f77492
malli <mallikarjun.bandi@gmail.com>
2019-12-27 21:14:46 +0100

    exception handler

--- src/main/java/com/practice/sakthi_via/exception/GlobalExceptionHandler.java
+++ src/main/java/com/practice/sakthi_via/exception/GlobalExceptionHandler.java
@@ -6,12 +6,20 @@ import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.Set;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(TransactionSystemException.class)
     public final ResponseEntity<Object> handleConstraintViolationException(Exception e){
-        String exceptionResponse = String.format("Invalid input parameters: %s\n", e.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponse);
+        Throwable cause = (TransactionSystemException) e.getCause();
+        if(cause instanceof ConstraintViolationException){
+            Set<ConstraintViolation<?>> constraintViolations = ((ConstraintViolationException) cause).getConstraintViolations();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(constraintViolations);
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
