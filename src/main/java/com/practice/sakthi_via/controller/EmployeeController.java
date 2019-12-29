a11f392232cc684530bcf316317fc63861c804e2
malli <mallikarjun.bandi@gmail.com>
2019-12-29 17:56:48 +0100

    added loggers

--- src/main/java/com/practice/sakthi_via/controller/EmployeeController.java
+++ src/main/java/com/practice/sakthi_via/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import com.practice.sakthi_via.model.Employee;
 import com.practice.sakthi_via.repository.EmployeeRepository;
 import com.practice.sakthi_via.service.EmployeeService;
 import io.swagger.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
@@ -15,7 +17,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Email;
 import java.util.List;
 
 import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.contains;
@@ -31,10 +32,13 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+
     @ApiOperation("Retrieve all the employees")
     @GetMapping("/employees")
     public ResponseEntity<List> getEmployee() {
         List<Employee> employeeList = employeeRepository.findAll();
+        logger.debug("List of employees :" + employeeList);
         return ResponseEntity.status(HttpStatus.OK).body(employeeList);
     }
 
@@ -117,11 +121,11 @@ public class EmployeeController {
             @ApiParam(value = "Email object to update", required = true)
             @Valid @RequestBody Employee employee) throws ResourceNotFoundException {
         Employee updatedEmployee = employeeRepository.findById(id)
-                .map(emp-> {
-                        emp.setUsername(employee.getUsername());
-                        emp.setName(employee.getUsername());
-                        emp.setEmail(employee.getEmail());
-                        emp.setAge(employee.getAge());
+                .map(emp -> {
+                    emp.setUsername(employee.getUsername());
+                    emp.setName(employee.getUsername());
+                    emp.setEmail(employee.getEmail());
+                    emp.setAge(employee.getAge());
                     return employeeRepository.save(employee);
                 })
                 .orElseThrow(() -> new ResourceNotFoundException("Employee ID " + id + " not found"));
