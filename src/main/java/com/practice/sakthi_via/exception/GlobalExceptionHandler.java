cab162335d541a0952a4ac356ee1c62bd11466e0
malli <mallikarjun.bandi@gmail.com>
2019-12-30 20:17:32 +0100

    updated global exception handler

--- src/main/java/com/practice/sakthi_via/exception/GlobalExceptionHandler.java
+++ src/main/java/com/practice/sakthi_via/exception/GlobalExceptionHandler.java
@@ -11,8 +11,20 @@ import java.util.Date;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<?> resourceNotFoundException(Exception e, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), e.getMessage(), webRequest.getDescription(false));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDetails);
+    }
+
+    @ExceptionHandler(UserNameExistsException.class)
+    public final ResponseEntity<?> userNameExistsException(Exception e, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), e.getMessage(), webRequest.getDescription(false));
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorDetails);
+    }
+
     @ExceptionHandler(Exception.class)
-    public final ResponseEntity<?> globalExceptionHandler(Exception e, WebRequest webRequest){
+    public final ResponseEntity<?> globalExceptionHandler(Exception e, WebRequest webRequest) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), e.getMessage(), webRequest.getDescription(false));
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetails);
     }
