2a4d8e27630ce9dc3c37a8316aedc77dc4f8369c
malli <mallikarjun.bandi@gmail.com>
2019-12-31 19:15:33 +0100

    updated for update query

--- src/main/java/com/practice/sakthi_via/repository/EmployeeRepository.java
+++ src/main/java/com/practice/sakthi_via/repository/EmployeeRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +18,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Employee findByUsername(String username);
 
+    @Transactional
     @Modifying
     @Query("UPDATE Employee e SET e.email = :email where e.id = :id")
     Integer updateEmployeeEmail(@Param("id") Long id, @Param("email") String email);
