bac9c5c6dd76e2fca73c5d0a73688a188da5bab3
malli <mallikarjun.bandi@gmail.com>
2019-12-31 17:55:30 +0100

    updated for validation

--- src/main/java/com/practice/sakthi_via/exception/GlobalExceptionHandler.java
+++ src/main/java/com/practice/sakthi_via/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -28,7 +29,7 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorDetails);
     }
 
-    @ExceptionHandler(ConstraintViolationException.class)
+    @ExceptionHandler({ConstraintViolationException.class, MethodArgumentNotValidException.class})
     public final ResponseEntity<?> constraintViolationException(Exception e, WebRequest webRequest) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), e.getMessage(), webRequest.getDescription(false));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDetails);
@@ -36,7 +37,6 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<?> globalExceptionHandler(Exception e, WebRequest webRequest) {
-        e.printStackTrace();
         ErrorDetails errorDetails = new ErrorDetails(new Date(), e.getMessage(), webRequest.getDescription(false));
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetails);
     }
