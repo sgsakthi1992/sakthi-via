218508385174341450a576698f42a840ac5b3adf
malli <mallikarjun.bandi@gmail.com>
2020-01-01 20:39:13 +0100

    updated code

--- src/main/java/com/practice/sakthi_via/validator/UsernameValidator.java
+++ src/main/java/com/practice/sakthi_via/validator/UsernameValidator.java
@@ -2,12 +2,10 @@ package com.practice.sakthi_via.validator;
 
 import com.practice.sakthi_via.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-@Component
 public class UsernameValidator implements ConstraintValidator<Username, String> {
 
     @Autowired
