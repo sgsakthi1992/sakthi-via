df980a99186dc433948ae0a7a82c5b2914c81376
sgsakthi1992 <sgsakthi1992@gmail.com>
2020-01-13 17:25:33 +0100

    Currency converter

--- src/test/java/com/practice/sakthi_via/unit/CurrencyConverterFacadeTest.java
+++ src/test/java/com/practice/sakthi_via/unit/CurrencyConverterFacadeTest.java
@@ -4,14 +4,23 @@
 
 package com.practice.sakthi_via.unit;
 
+import com.practice.sakthi_via.facade.CurrencyConverterFacade;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
-@ExtendWith(SpringExtension.class)
 class CurrencyConverterFacadeTest {
+
+    CurrencyConverterFacade currencyConverterFacade = new CurrencyConverterFacade();
+
+    CurrencyConverterFacade spyCurrencyConverterFacade = spy(currencyConverterFacade);
 
     @Test
     void getCountriesAndCurrencies() {
@@ -19,5 +28,23 @@ class CurrencyConverterFacadeTest {
 
     @Test
     void getCurrencyRate() {
+    }
+
+    @Test
+    void getHighestAndLowestCurrencyRate() {
+    }
+
+    @Test
+    void getCountryForCurrencyCode() {
+        //GIVEN
+        HashMap<String, String> countries = new HashMap<>();
+        countries.put("HUF", "Hungarian Forint");
+        countries.put("INR", "Indian Rupee");
+        countries.put("USD", "US Dollar");
+        when(spyCurrencyConverterFacade.getCountriesAndCurrencies()).thenReturn(countries);
+        //WHEN
+        String country = spyCurrencyConverterFacade.getCountryForCurrencyCode("HUF");
+        //THEN
+        assertEquals("Hungarian Forint", country);
     }
 }
